9029768f1ec9ede4a27d4d8135ee8f055b4f49bc
BPTPW <bptpw_service@163.com>
2023-07-21 18:02:12 +0800

    first commit

---
+++ app/src/main/java/com/bptpw/tvbrowser/browser_activity.kt
@@ -0,0 +1,11 @@
+package com.bptpw.tvbrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class browser_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browser)
+    }
+}
